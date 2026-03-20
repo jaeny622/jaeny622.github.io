968352497a34187ab46d2bbcc3f6611f669ef48f
jisu <jisu_oh@3ssoft.co.kr>
2026-03-20 15:15:59 +0900

    [chore] Skills 배치 및 About styled component 명 변경

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -7,12 +7,13 @@ import { ABOUT_CARDS } from '@/data/portfolio';
 
 export default function About() {
   const ref = useReveal();
+
   return (
     <section id="about">
       <C.SectionWrap>
         <C.Eyebrow>About Me</C.Eyebrow>
         <C.SectionTitle>안녕하세요,<br/><em>오지수</em>입니다.</C.SectionTitle>
-        <S.Grid ref={ref}>
+        <S.GridBox ref={ref}>
           <div>
             <S.TopComment>
               즉각적인 변화를 눈으로 볼 수 있는<br/>
@@ -26,24 +27,24 @@ export default function About() {
               빠른 이슈 대응 능력과 다른 직군과의 원활한 소통이 좋은 서비스를 만드는
               핵심이라고 생각하며, 능동적으로 팀원들과 협력하는 개발자가 되고자 합니다.
             </S.BodyComment>
-            <S.Contact>
-              <S.Item><S.ItemLabel>email</S.ItemLabel><S.ItemValue><a href="mailto:jaeny96@naver.com">jaeny96@naver.com</a></S.ItemValue></S.Item>
-              <S.Item><S.ItemLabel>phone</S.ItemLabel><S.ItemValue>010-6641-9235</S.ItemValue></S.Item>
-              <S.Item><S.ItemLabel>birth</S.ItemLabel><S.ItemValue>1996.10.16</S.ItemValue></S.Item>
-            </S.Contact>
           </div>
           <S.CardBox className='js-reveal' style={{ transitionDelay:'100ms' }}>
+            <S.Contact>
+              <S.AboutItem><S.ItemLabel>email</S.ItemLabel><S.ItemValue><a href="mailto:jaeny96@naver.com">jaeny96@naver.com</a></S.ItemValue></S.AboutItem>
+              <S.AboutItem><S.ItemLabel>phone</S.ItemLabel><S.ItemValue>010-6641-9235</S.ItemValue></S.AboutItem>
+              <S.AboutItem><S.ItemLabel>birth</S.ItemLabel><S.ItemValue>1996.10.16</S.ItemValue></S.AboutItem>
+            </S.Contact>
             {ABOUT_CARDS.map(c => (
-              <S.Card key={c.title} data-cur="">
+              <S.AboutCard key={c.title} data-cur="">
                 <S.Icon>{c.icon}</S.Icon>
                 <div>
                   <S.CardTitle>{c.title}</S.CardTitle>
                   <S.CardDesc>{c.desc}</S.CardDesc>
                 </div>
-              </S.Card>
+              </S.AboutCard>
             ))}
           </S.CardBox>
-        </S.Grid>
+        </S.GridBox>
       </C.SectionWrap>
     </section>
   )
