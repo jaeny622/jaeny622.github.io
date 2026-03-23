e48d96a6ec29b010d3022b4692b53b9018cb586a
jisu <jisu_oh@3ssoft.co.kr>
2026-03-23 10:59:31 +0900

    [feat] Experience 영역 구현 - Experience, 주요 Project 카드 구현 - Static 데이터, 타입 추가

---
+++ src/components/sections/Experience.tsx
@@ -0,0 +1,43 @@
+import { useReveal } from '@/hooks/useReveal';
+
+import * as C from '@/styles/Common.styles';
+import * as S from '@/components/sections/Experience.styles';
+
+import { EXPERIENCES } from '@/data/portfolio';
+
+export default function Experience() {
+  const ref = useReveal('.js-reveal')
+  return (
+    <section id="experience">
+      <C.SectionWrap>
+        <C.Eyebrow>Work Experience</C.Eyebrow>
+        <C.SectionTitle>경력 <em>사항</em></C.SectionTitle>
+        <S.ExpList ref={ref}>
+          {EXPERIENCES.map((exp, expIdx) => (
+            <S.ExpCardBox key={exp.company} className='js-reveal' style={{ transitionDelay: `${expIdx * 100}ms` }}>
+              <S.CoCard>
+                <S.CoName>{exp.company}</S.CoName>
+                <S.CoPeriod>{exp.period}</S.CoPeriod>
+                <S.CoRole>
+                  {exp.role.split('\n').map((l, i) => <span key={i}>{l}<br /></span>)}
+                  </S.CoRole>
+              </S.CoCard>
+              <S.Projects>
+                {exp.projects.map(proj => (
+                  <S.ProjectCard key={proj.name} data-hover="">
+                    <S.ProjName>{proj.name}</S.ProjName>
+                    <S.ProjPeriod>{proj.period}</S.ProjPeriod>
+                    <S.ProjDesc>{proj.desc}</S.ProjDesc>
+                    <S.TagBox>
+                      {proj.tags.map(tag => <S.Tag key={tag}>{tag}</S.Tag>)}
+                    </S.TagBox>
+                  </S.ProjectCard>
+                ))}
+              </S.Projects>
+            </S.ExpCardBox>
+          ))}
+        </S.ExpList>
+      </C.SectionWrap>
+    </section>
+  )
+}
