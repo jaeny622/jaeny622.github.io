9a541d539ceedf2e32de3b72be234952d13b2e9b
jisu <jisu_oh@3ssoft.co.kr>
2026-03-24 14:08:15 +0900

    [feat] Footer 영역 구현 - App 내 Footer 배치

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,10 @@
+import * as S from '@/components/layout/Footer.styles';
+
+
+export default function Footer() {
+  return (
+    <S.Footer>
+      <S.CopyWriter>© 2026 오지수. All Rights Reserved.</S.CopyWriter>
+    </S.Footer>
+  )
+}
